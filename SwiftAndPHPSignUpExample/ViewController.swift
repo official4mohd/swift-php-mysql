acb6579af6efd61c45cbd8179ea75e514e93e806
Sergey Kargopolov <skargopolov@central1.com>
2016-09-23 12:59:59 -0700

    Converted to Swift 3

--- SwiftAndPHPSignUpExample/ViewController.swift
+++ SwiftAndPHPSignUpExample/ViewController.swift
@@ -11,121 +11,122 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var userEmailAddressTextField: UITextField!
     @IBOutlet weak var userPasswordTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func signInButtonTapped(sender: AnyObject) {
-       
+    
+    @IBAction func signInButtonTapped(_ sender: AnyObject) {
+        
         let userEmailAddress = userEmailAddressTextField.text
         let userPassword = userPasswordTextField.text
         
         
         if(userEmailAddress!.isEmpty || userPassword!.isEmpty)
         {
-          // Display an alert message
-            let myAlert = UIAlertController(title: "Alert", message:"All fields are required to fill in", preferredStyle: UIAlertControllerStyle.Alert);
-            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:nil)
+            // Display an alert message
+            let myAlert = UIAlertController(title: "Alert", message:"All fields are required to fill in", preferredStyle: UIAlertControllerStyle.alert);
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:nil)
             myAlert.addAction(okAction);
-            self.presentViewController(myAlert, animated: true, completion: nil)
+            self.present(myAlert, animated: true, completion: nil)
             return
         }
         
         
-        let spinningActivity = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        spinningActivity.labelText = "Loading"
-        spinningActivity.detailsLabelText = "Please wait"
+        let spinningActivity = MBProgressHUD.showAdded(to: self.view, animated: true)
+        spinningActivity?.labelText = "Loading"
+        spinningActivity?.detailsLabelText = "Please wait"
         
         
-     let myUrl = NSURL(string: "http://localhost/SwiftAppAndMySQL/scripts/userSignIn.php");
-       
-    let request = NSMutableURLRequest(URL:myUrl!);
-        request.HTTPMethod = "POST";
+        let myUrl = URL(string: "http://localhost/SwiftAppAndMySQL/scripts/userSignIn.php");
         
-     let postString = "userEmail=\(userEmailAddress!)&userPassword=\(userPassword!)";
+        var request = URLRequest(url:myUrl!);
+        request.httpMethod = "POST";
         
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        let postString = "userEmail=\(userEmailAddress!)&userPassword=\(userPassword!)";
         
+        request.httpBody = postString.data(using: String.Encoding.utf8);
         
-        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
-           
-            dispatch_async(dispatch_get_main_queue())
-            {
-                
-                spinningActivity.hide(true)
-                
-                
-               if(error != nil)
-               {
-                  //Display an alert message
-                let myAlert = UIAlertController(title: "Alert", message: error!.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert);
-                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:nil)
-                myAlert.addAction(okAction);
-                self.presentViewController(myAlert, animated: true, completion: nil)
-                 return
-               }
-                
-                
-                
-                do {
-                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
-                
-                if let parseJSON = json {
-                 
-                    let userId = parseJSON["userId"] as? String
-                    if(userId != nil)
-                    {
-                        
-                       NSUserDefaults.standardUserDefaults().setObject(parseJSON["userFirstName"], forKey: "userFirstName")
-                        NSUserDefaults.standardUserDefaults().setObject(parseJSON["userLastName"], forKey: "userLastName")
-                       NSUserDefaults.standardUserDefaults().setObject(parseJSON["userId"], forKey: "userId")
-                        NSUserDefaults.standardUserDefaults().synchronize()
-                        
-                       // take user to a protected page
-                        /*
-                        let mainPage = self.storyboard?.instantiateViewControllerWithIdentifier("MainPageViewController") as! MainPageViewController
-                        
-                        let mainPageNav = UINavigationController(rootViewController: mainPage)
-                        let appDelegate = UIApplication.sharedApplication().delegate
-                        
-                        appDelegate?.window??.rootViewController = mainPageNav
-                  */
-                        
-                     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-                        
-                        appDelegate.buildNavigationDrawer()
-                        
-                        
-                    } else {
-                      // display an alert message
-                        let userMessage = parseJSON["message"] as? String
-                        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert);
-                        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:nil)
-                        myAlert.addAction(okAction);
-                        self.presentViewController(myAlert, animated: true, completion: nil)
-                    }
-                
-                    }
-                } catch
+        let task = URLSession.shared.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            DispatchQueue.main.async
                 {
-                   print(error)
-                }
-                
-                
+                    
+                    spinningActivity!.hide(true)
+                    
+                    
+                    if(error != nil)
+                    {
+                        //Display an alert message
+                        let myAlert = UIAlertController(title: "Alert", message: error!.localizedDescription, preferredStyle: UIAlertControllerStyle.alert);
+                        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:nil)
+                        myAlert.addAction(okAction);
+                        self.present(myAlert, animated: true, completion: nil)
+                        return
+                    }
+                    
+                    
+                    
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                        
+                        if let parseJSON = json {
+                            
+                            let userId = parseJSON["userId"] as? String
+                            if(userId != nil)
+                            {
+                                
+                                UserDefaults.standard.set(parseJSON["userFirstName"], forKey: "userFirstName")
+                                UserDefaults.standard.set(parseJSON["userLastName"], forKey: "userLastName")
+                                UserDefaults.standard.set(parseJSON["userId"], forKey: "userId")
+                                UserDefaults.standard.synchronize()
+                                
+                                // take user to a protected page
+                                /*
+                                 let mainPage = self.storyboard?.instantiateViewControllerWithIdentifier("MainPageViewController") as! MainPageViewController
+                                 
+                                 let mainPageNav = UINavigationController(rootViewController: mainPage)
+                                 let appDelegate = UIApplication.sharedApplication().delegate
+                                 
+                                 appDelegate?.window??.rootViewController = mainPageNav
+                                 */
+                                
+                                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                                
+                                appDelegate.buildNavigationDrawer()
+                                
+                                
+                            } else {
+                                // display an alert message
+                                let userMessage = parseJSON["message"] as? String
+                                let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.alert);
+                                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:nil)
+                                myAlert.addAction(okAction);
+                                self.present(myAlert, animated: true, completion: nil)
+                            }
+                            
+                        }
+                    } catch
+                    {
+                        print(error)
+                    }
+                    
+                    
             }
             
             
             
-        }).resume()
+        }
+        
+        task.resume()
         
     }
-
+    
 }
 
